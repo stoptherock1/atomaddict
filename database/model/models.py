d03a4db07721319d7ad772c0e6b5bce15a14c36d
piotrlechowicz <piotr.tobiasz.lechowicz@gmail.com>
2015-04-13 02:23:29 +0200

    Database version 1

--- database/model/models.py
+++ database/model/models.py
@@ -1,9 +1,70 @@
 from database.model import db
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Table, DateTime
+from sqlalchemy.orm import relationship
 
+
+users_tags = Table('users_tags', db.metadata, 
+                   Column('user_id', Integer, ForeignKey('users.id')),
+                   Column('tag_id', Integer, ForeignKey('tags.id')))
+
+users_articles = Table('users_articles', db.metadata,
+                       Column('user_id', Integer, ForeignKey('users.id')),
+                       Column('article_id', Integer, ForeignKey('articles.id')))
+                    
 class User(db.Model):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(120), unique=True, nullable = False)
+    email = Column(String(120), nullable = False)
     nickname = Column(String(64))
     password = Column(String(50), nullable = False)
+    # Many to many Users <-> Tags
+    tags = relationship('Tag', secondary = users_tags, backref = 'users', lazy = 'dynamic')
     
+    # Many to many Users <-> unviewed articles
+    articles = relationship('Article', secondary = users_articles, backref = 'users', lazy = 'dynamic')
+    
+    def __repr__(self):
+        return "<User (email = '%s', nickname = '%s', password = '%s')>" % \
+            (self.email, self.nickname, self.password)
+    
+    
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(30), unique = True)
+    
+    # One to Many Tag -> Websites
+    websites = relationship('Website', backref='tag')
+    
+    def __repr__(self):
+        return "<Tag (name = '%s')>" % self.name
+    
+class Website(db.Model):
+    __tablename__ = 'websites'
+    id = Column(Integer, primary_key = True)
+    uri = Column(Text, nullable = False)
+    name = Column(String(120))
+    
+    # Many to One: websites -> tag
+    tag_id = Column(Integer, ForeignKey('tags.id'))
+    
+    # One to Many: website -> articles
+    articles = relationship('Article', backref='website')
+    
+    def __repr__(self):
+        return "<Website (name = '%s', uri = '%s', tag = '%s')>" % (self.name, self.uri, self.tag_id.name)
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = Column(Integer, primary_key = True)
+    head = Column(Text())
+    content = Column(Text())
+    picture = Column(Text())
+    time = Column(DateTime)
+    
+    # Many to One: articles -> website
+    website_id = Column(Integer, ForeignKey('websites.id'))
+    
+    def __repr__(self):
+        return "<Article (head = '%s', content = '%s', date = '%s', website = '%s')>" % \
+            (self.head, self.content, self.time, self.website_id.name)
