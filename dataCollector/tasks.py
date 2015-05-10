0271e1d485461fb9f933e5b99e155b973f9b9cc5
str1 <str1@MBP.local>
2015-05-10 23:23:57 +0200

    Basic celery functionality added:
    1) RSS feeds could be fetched and parsed in celery task. Result can be processed inside of the original app.
    2) Celery's tasks are located in the tasks.py file

---
+++ dataCollector/tasks.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from celery import Celery
+import feedparser
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+##main
+flask_app = Flask(__name__)
+
+flask_app.config['CELERY_BROKER_URL'] = 'amqp://guest@localhost//'
+flask_app.config['CELERY_RESULT_BACKEND'] = 'amqp'
+
+celery = make_celery(flask_app)
+celery.conf.update(flask_app.config)
+
+
+
+############ tasks
+@celery.task
+def downloadFeeds(url):
+    data = feedparser.parse(url)
+    return data
