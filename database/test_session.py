07762ee076b4d03540d84d0145f8ae94d5a70a37
Piotr Lechowicz <piotr.tobiasz.lechowicz@gmail.com>
2015-05-11 13:12:54 +0200

    changing test_session.py

--- database/test_session.py
+++ database/test_session.py
@@ -3,11 +3,7 @@ from database.session import Delete, Get, Put, Add
 
 if __name__ == "__main__":
 
-    
-    
-    
-    
-    
+
     # delete all
 
     delete = Delete()
@@ -17,21 +13,15 @@ if __name__ == "__main__":
     delete.all_tags()
     delete.all_articles()
     delete.close_session()
-    
     # You can delete also by delete.user(email='...')
-
     # put some things
-    
-
     put = Put()
-    put.user(email='Alice@gmail', password='Alicepass', nickname='Alice')
-    put.user(email='George@gmail', password='George', nickname='George')
-    put.user(email='Adam@gmail', password='Adam', nickname='Adam')
-    put.user(email='Bob@gmail', password='Bob', nickname='')
-    put.user(email='Margaret@gmail', password='Margaret', nickname='Margaret')
-    
+    user = put.user(email='piotrek@gmail', password='piotrek', nickname='Piotrek')
+
+
+
 #    W zasadzie to mozna by tak:
-#    
+#
 #     put = Put()
 #     try:
 #         put.user(...)
@@ -40,43 +30,55 @@ if __name__ == "__main__":
 #         Handle error -> 
 #         Poprzez print error dostaniesz odpowiedz typu 'already exist', albo 
 #                         'not all parameters are passed' itp
-# 
-
-
-
+#
+    cnn = put.website(uri='http://rss.cnn.com/rss/cnn_topstories.rss',
+                name='cnn.com')
+    nyt = put.website(uri='http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+                name='New York Times')
+    gadget_of_the_week = put.website(uri='http://feeds.feedburner.com/time/gadgetoftheweek', name='Gadget of the Week')
+    world_news = put.tag(name='Worlds News')
     
+    
+    science = put.tag(name='Science')
 
-    put.website(uri='google.com', name='google')
-    put.website(uri='wp.pl', name='wp')
-    put.website(uri='premierleague.com', name='liga angielska')
-    put.website(uri='pudelek.pl', name=None)
+#     put.tag(name='sport')
+#     put.tag(name='smieci')
 
-    put.tag(name='informacje')
-    put.tag(name='sport')
-    put.tag(name='smieci')
-
-    a1 = put.article(head='super wygrana', content='super wygrana druzyny')
-    a2 = put.article(head='paris hilton', content='paris hilton na pokazie')
-    a3 = put.article(head='paris asdfhilton', content='asdfparis hilton na ' +
-                     'pokazie')
-    a4 = put.article(head='pozar', content='wielkipozar!')
+#     a1 = put.article(head='Chelsea wygrala z Liverpoolem', content='super wygrana druzyny')
+#     a2 = put.article(head='paris hilton', content='paris hilton na pokazie')
+#     a3 = put.article(head='paris asdfhilton', content='asdfparis hilton na ' +
+#                      'pokazie')
+#     a4 = put.article(head='pozar', content='wielkipozar!')
+    a1 = put.article(head='33 million in U.S. under severe weather threat',
+                     uri='adsf')
+    a2 = put.article(head='Tropical Storm Ana notches down as it hits South Carolina',
+                     uri='adsfdfsdf')
+    a3 = put.article(head='3DConnexion SpaceNavigator', 
+                     uri='http://content.time.com/time/business/article/0,8599,1573203,00.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+time%2Fgadgetoftheweek+%28TIME%3A+Gadget+of+the+Week%29&utm_content=FeedBurner',
+                      picture='http://img.timeinc.net/time/daily/2007/facelift/360_space_0102.jpg')
+    
+    a4 = put.article(head='Panasonic Plasma and More',
+                     uri='http://content.time.com/time/business/article/0,8599,1571909,00.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+time%2Fgadgetoftheweek+%28TIME%3A+Gadget+of+the+Week%29&utm_content=FeedBurner',
+                     picture='http://img.timeinc.net/time/daily/2006/facelift/360_panasonic42inchplasma.jpg')
+    a5 = put.article(head='Nikon D40 Digital SLR Camera',
+                     uri='http://content.time.com/time/business/article/0,8599,1566933,00.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+time%2Fgadgetoftheweek+%28TIME%3A+Gadget+of+the+Week%29&utm_content=FeedBurner')
     put.close_session()
 
     # add some things
 
     add = Add()
-    add.website_to_tag(tagname='informacje', website_uri='google.com')
-    add.website_to_tag(tagname='sport', website_uri='premierleague.com')
-    add.website_to_tag(tagname='informacje', website_uri='wp.pl')
-    add.website_to_tag(tagname='smieci', website_uri='pudelek.pl')
+    add.website_to_tag(tagname=world_news, website_uri=cnn)
+    add.website_to_tag(tagname=world_news, website_uri=nyt)
+    add.website_to_tag(tagname=science, website_uri=gadget_of_the_week)
 
-    add.tag_to_user(email='Adam@gmail', tagname='informacje')
-    add.tag_to_user(email='Bob@gmail', tagname='informacje')
-    add.article_to_website(website_uri='wp.pl', article_id=a4)
-    add.article_to_website(website_uri='google.com', article_id=a4)
-    add.article_to_website(website_uri='premierleague.com', article_id=a1)
-    add.article_to_website(website_uri='pudelek.pl', article_id=a2)
-    add.article_to_website(website_uri='pudelek.pl', article_id=a3)
+    add.article_to_website(website_uri=cnn, article_uri=a1)
+    add.article_to_website(website_uri=cnn, article_uri=a2)
+#     add.article_to_website(website_uri='premierleague.com', article_uri=a1)
+#     add.article_to_website(website_uri='pudelek.pl', article_uri=a2)
+#     add.article_to_website(website_uri='pudelek.pl', article_uri=a3)
+    add.article_to_website(website_uri=gadget_of_the_week, article_uri=a3)
+    add.article_to_website(website_uri=gadget_of_the_week, article_uri=a4)
+    add.article_to_website(website_uri=gadget_of_the_week, article_uri=a5)
     add.close_session()
 
     # get some things
@@ -89,25 +91,49 @@ if __name__ == "__main__":
     for w1 in websites:
         print w1.tag
 
-    tags = get.tag('informacje')
+    tags = get.tag(world_news)
+    print tags
     print tags.websites
+    for web in tags.websites:
+        print web.articles
 
-    articles = get.all_articles()
-    print articles
-    for a in articles:
-        print a.website
-        print a.users
+#     articles = get.all_articles()
+#     print articles
+#     for a in articles:
+#         print a.website
+#         print a.users
 
-    user = get.user(email='Adam@gmail')
-    print "\n", user
-    for t in user.tags:
-        print t
-        for w in t.websites:
-            print w
-            for a in w.articles:
-                print a
+#     user = get.user(email='maia_user@gmail')
+#     print "\n", user
+#     for t in user.tags:
+#         print t
+#         for w in t.websites:
+#             print w
+#             for a in w.articles:
+#                 print a
 
     get.close_session()
+    
+    add = Add()
+    add.tag_to_user(user, world_news)
+    add.tag_to_user(user, science)
+    
+    add.close_session()
+    
+    get = Get()
+    user = get.user(email=user)
+    print user
+    print user.tags
+    if user.tags:
+        for tag in user.tags:
+            print tag
+            if tag.websites:
+                for web in tag.websites:
+                    print web
+            
+    
+    get.close_session()
+    
  
  
  # wypis z konsoli   
@@ -191,4 +217,3 @@ if __name__ == "__main__":
 # #     #------------------------------------------
 # #     
 #        
-#===============================================================================
